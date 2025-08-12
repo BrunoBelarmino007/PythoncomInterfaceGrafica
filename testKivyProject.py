3276756c3244b720890cd0a5a385efb13dbf73d0
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-08-11 22:14:08 -0300

    Foi adicionado uma forma diferente de retorno, sendo agora com o retorno de uma imagem

--- testKivyProject.py
+++ testKivyProject.py
@@ -1,13 +1,22 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.image import AsyncImage
 
 class MainApp ( App ):
-    def build ( self ):
-        label = Label ( text = 'Fala galera!',
-            size_hint = ( .5, .5 ),
-            pos_hint = { 'center_x': .5, 'center_y': .5 })
+    # def build ( self ):
+    #     label = Label ( text = 'Fala galera!',
+    #         size_hint = ( .5, .5 ),
+    #         pos_hint = { 'center_x': .5, 'center_y': .5 })
         
-        return label
+    #     return label
+    
+    def build ( self ):
+        img = AsyncImage ( source = 'https://supermariorun.com/assets/img/stage/mario03.png' ,
+        # img = AsyncImage ( source = 'https://icon2.cleanpng.com/lnd/20250316/iq/c119a72deeaa4766f05807551ddc6e.webp' ,
+        # img = AsyncImage ( source = 'https://i.pinimg.com/736x/10/51/7f/10517f8dbcbf03227ada14a72e8f502c.jpg' ,
+            size_hint = ( 1 , .5 ),
+            pos_hint = { 'center_x': .5 , 'center_y': .5 })
+        return img
 
 if __name__ == '__main__' :
     app = MainApp()
