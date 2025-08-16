4d5ffda41902599f72ed954c6b144071557198d9
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-08-15 23:41:48 -0300

    Foi adicionado interação na classe Esfera

--- modulo_2/Esfera.py
+++ modulo_2/Esfera.py
@@ -8,19 +8,24 @@ class Esfera:
 
     def volume(self):
         vol = (4/3) * math.pi * (self.raio ** 3)
-
         return vol
     
     def area(self):
         ar = 4 * math.pi * (self.raio ** 2)
-
         return ar
 
-bola = Esfera('vermelha', 4)
-bola2 = Esfera('azul', 7)
+cor1 = input("Informe a cor da primeira esfera: ")
+raio1 = float(input("Informe o raio da primeira esfera (em cm): "))
 
-print(f"O volume da esfera de cor {bola.cor} é: {bola.volume()} cm^3")
+cor2 = input("Informe a cor da segunda esfera: ")
+raio2 = float(input("Informe o raio da segunda esfera (em cm): "))
+
+bola = Esfera(cor1, raio1)
+bola2 = Esfera(cor2, raio2)
+
+# Resultados
+print(f"\nO volume da esfera de cor {bola.cor} é: {bola.volume()} cm^3")
 print(f"A área da esfera de cor {bola.cor} é: {bola.area()} cm^2")
 
-print(bola.volume())
-print(Esfera.volume(bola))  # Acessando o método volume do objeto bola
+print(f"\nO volume da esfera de cor {bola2.cor} é: {bola2.volume()} cm^3")
+print(f"A área da esfera de cor {bola2.cor} é: {bola2.area()} cm^2")
